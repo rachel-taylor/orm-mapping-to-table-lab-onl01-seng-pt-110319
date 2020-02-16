750fdf107e79eb6ff51e5f8e3ad9ba3530e5a893
Rachel T <rachmaybe@gmail.com>
2020-02-16 01:49:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,12 +29,11 @@ class Student
   end
 
   def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.grade)
+     sql = <<-SQL
+  insert into students (name,grade) values(?,?);
+  SQL
+  DB[:conn].execute(sql, self.name, self.grade)
+  @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students")[0][0]
   end
 
   def self.create(name:, grade:)
